b66fd9ba5b943810504762fa7b53c83e6e5bab30
Mohit Ashiliya <mohit.ashliya@gmail.com>
2022-03-18 22:57:19 +0530

    initialised

---
+++ script.js
@@ -0,0 +1,7 @@
+const child = document.getElementById('buttonDark').lastElementChild
+
+document.querySelector('share').appendChild(child)
+
+document.getElementById('shareButton').addEventListener('click', () =>{
+    shareButton.style.display = "none";
+})
